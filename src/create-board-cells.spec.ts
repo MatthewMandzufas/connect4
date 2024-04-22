32f4dd55701b1c93eb6399c2b376abc8ac7db01d
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-04-22 15:51:59 +0800

    RED: Create-cells, given rows, columns and a player selection strategy, that always selects player 1, creates a row x column board filled with player 1 tokens

--- src/create-board-cells.spec.ts
+++ src/create-board-cells.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import createBoardCells from "@/create-board-cells";
+import createBoardCells from "./create-board-cells";
 
 describe("create-cells", () => {
   it("returns a 0x0 board given defaults", () => {
@@ -36,5 +36,35 @@ describe("create-cells", () => {
 
     expect(board.length).toEqual(6);
     expect(board[0].length).toEqual(10);
+  });
+  describe("given rows, columns and a player selection strategy", () => {
+    describe("that always selects player 1", () => {
+      it("creates a row x column board filled with player 1 tokens", () => {
+        const playerOneSelectionStrategy: () => 1 | 2 | undefined = () => 1;
+        const board = createBoardCells(2, 2, playerOneSelectionStrategy);
+        expect(board).toEqual([
+          [
+            expect.objectContaining({
+              player: 1,
+              uuid: expect.toBeUUID(),
+            }),
+            expect.objectContaining({
+              player: 1,
+              uuid: expect.toBeUUID(),
+            }),
+          ],
+          [
+            expect.objectContaining({
+              player: 1,
+              uuid: expect.toBeUUID(),
+            }),
+            expect.objectContaining({
+              player: 1,
+              uuid: expect.toBeUUID(),
+            }),
+          ],
+        ]);
+      });
+    });
   });
 });
