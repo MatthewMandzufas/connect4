61b7b009af1120b805cc0ee1213fc2db465a9196
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-05-07 15:18:58 +0800

    GREEN: to-ascii-table > given a 1 row grid > and 1 column > with content greater than 1 character in length > returns a 1x1 ascii table

--- src/connect-4-domain/to-ascii-table.spec.ts
+++ src/connect-4-domain/to-ascii-table.spec.ts
@@ -19,12 +19,12 @@ describe("to-ascii-table", () => {
 `);
       });
       describe("with content greater than 1 character in length", () => {
-        it("returns to a 1x1 ascii table", () => {
+        it("returns a 1x1 ascii table", () => {
           const asciiTable = toAsciiTable([["12"]]);
           expect(asciiTable).toStrictEqual(`
 |----|
 | 12 |
-|----| 
+|----|
 `);
         });
       });
--- src/connect-4-domain/to-ascii-table.ts
+++ src/connect-4-domain/to-ascii-table.ts
@@ -1,15 +1,24 @@
 function toAsciiTable(grid: Array<Array<string>>): string {
+  if (grid.length === 0) {
+    return "";
+  }
+  let largestWidth = 1;
   const table = grid.reduce((tableRows, currentRow) => {
     tableRows.push(
       currentRow.reduce((tableRow, currentElement) => {
+        largestWidth =
+          currentElement.length > largestWidth
+            ? currentElement.length
+            : largestWidth;
         return tableRow.concat(`| ${currentElement} |`);
       }, "")
     );
     return tableRows;
   }, []);
-  return grid.length !== 0
-    ? ["", "|---|", table[0], "|---|", ""].join("\n")
-    : "";
+
+  const padWidth = new Array(largestWidth + 2).fill("-");
+  const border = ["|", ...padWidth, "|"].join("");
+  return ["", border, table[0], border, ""].join("\n");
 }
 
 export default toAsciiTable;
