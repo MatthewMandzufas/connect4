486199e7dc0caa9b9b30c395e043efba67c4300b
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-05-07 14:34:35 +0800

    GREEN: to-ascii-table, given a 1 row grid > and 1 column > resolves to a 1x1 ascii table

--- src/connect-4-domain/to-ascii-table.ts
+++ src/connect-4-domain/to-ascii-table.ts
@@ -1,5 +1,15 @@
-function toAsciiTable(board) {
-  return "";
+function toAsciiTable<T>(grid: Array<Array<T>>): string {
+  const table = grid.reduce((table, currentRow) => {
+    const rowContent = currentRow.reduce((tableRow, currentElement) => {
+      return tableRow.concat(`
+|---|
+| ${currentElement} |
+|---|
+`);
+    }, "");
+    return table.concat(rowContent);
+  }, "");
+  return table;
 }
 
 export default toAsciiTable;
