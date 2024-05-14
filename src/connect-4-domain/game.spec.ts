64cf4af3bfa08752ccca7f42be6cdff882923aca
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-05-14 15:41:37 +0800

    RED/GREEN: game > new game > given defaults > creates a game where player 2 starts with a number of tokens, equal to half the number of cells

--- src/connect-4-domain/game.spec.ts
+++ src/connect-4-domain/game.spec.ts
@@ -46,6 +46,15 @@ describe("game", () => {
           })
         );
       });
+      it("creates a game where player 2 starts with a number of tokens, equal to half the number of cells", () => {
+        const game = new GameFactory();
+        expect(game.getPlayerStats(2)).toEqual(
+          expect.objectContaining({
+            remainingDisks: 21,
+            playerNumber: 2,
+          })
+        );
+      });
     });
   });
 });
