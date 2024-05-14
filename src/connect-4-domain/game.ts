b9bb6f36c8ce606ca61568387656e90f637ec55f
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-05-14 15:35:21 +0800

    GREEN: game > new game > given defaults > creates a game where player 1 starts with a number of tokens, equal to half the number of cells

--- src/connect-4-domain/game.ts
+++ src/connect-4-domain/game.ts
@@ -6,19 +6,40 @@ type GameParameters = {
   boardDimensions: { rows: number; columns: number };
 };
 
+interface PlayerStats {
+  playerNumber: 1 | 2;
+  remainingDisks: number;
+}
+
 interface Game {
   getBoard: () => Array<Array<BoardCell>>;
 }
 
 class GameFactory implements Game {
   private board: Array<Array<BoardCell>>;
+  private players: Record<1 | 2, PlayerStats>;
 
   constructor(
     { boardDimensions }: GameParameters = {
       boardDimensions: { rows: 6, columns: 7 },
     }
   ) {
+    const startingDisks = (boardDimensions.rows * boardDimensions.columns) / 2;
+    this.players = this.#createPlayers(startingDisks);
     this.board = this.#createBoard(boardDimensions);
+  }
+
+  #createPlayers(startingDisks: number): Record<1 | 2, PlayerStats> {
+    return {
+      1: {
+        playerNumber: 1,
+        remainingDisks: startingDisks,
+      },
+      2: {
+        playerNumber: 2,
+        remainingDisks: startingDisks,
+      },
+    };
   }
 
   #createBoard(boardDimensions: {
@@ -35,6 +56,10 @@ class GameFactory implements Game {
 
   getBoard() {
     return this.board;
+  }
+
+  getPlayerStats(playerNumber: 1 | 2): PlayerStats {
+    return this.players[playerNumber];
   }
 }
 
