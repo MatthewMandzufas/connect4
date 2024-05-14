da68ee18cf85f97e961c14ab1f059c41311a132b
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-05-14 16:33:25 +0800

    RED/GREEN: toBeDeeplyUnequal > it should pass, given an object and an array

--- src/connect-4-domain/to-be-deeply-unequal.spec.ts
+++ src/connect-4-domain/to-be-deeply-unequal.spec.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect } from "vitest";
+import toBeDeeplyUnequal from "./to-be-deeply-unequal";
 
 describe("toBeDeeplyUnequal", () => {
   it("should fail when given objects are the same object", () => {
@@ -10,5 +11,10 @@ describe("toBeDeeplyUnequal", () => {
     const firstObject = {};
     const secondObject = {};
     expect(firstObject).toBeDeeplyUnequal(secondObject);
+  });
+  it("it should pass, given an object and an array", () => {
+    const arr = [];
+    const obj = {};
+    expect(obj).toBeDeeplyUnequal(arr);
   });
 });
