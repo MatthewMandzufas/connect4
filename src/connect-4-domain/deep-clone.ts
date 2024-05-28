d6aa754a4c469a1513e7c139dedc6b523cce8c21
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-05-28 09:34:08 +0800

    GREEN: deepClone > Should return a deep copy of an array

--- src/connect-4-domain/deep-clone.ts
+++ src/connect-4-domain/deep-clone.ts
@@ -1,7 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 function deepClone<T>(value: T): T {
-  return value;
+  if (!(value instanceof Object)) {
+    return value;
+  }
+
+  let result: any;
+
+  if (Array.isArray(value)) {
+    result = [] as any;
+  } else {
+    result = {} as any;
+  }
+
+  for (const key of Object.keys(value)) {
+    result[key] = deepClone((value as { [key: string]: any })[key]);
+  }
+
+  return result;
 }
 
 export default deepClone;
