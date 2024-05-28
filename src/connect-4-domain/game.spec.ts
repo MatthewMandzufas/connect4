9cee4bba8a15c99ab333c949a16266894ff25d49
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-05-28 15:48:47 +0800

    RED: game > new game > returns the currently active player

--- src/connect-4-domain/game.spec.ts
+++ src/connect-4-domain/game.spec.ts
@@ -165,5 +165,9 @@ describe("game", () => {
         });
       });
     });
+    it("returns the currently active player", () => {
+      const game = new GameFactory();
+      expect(game.getActivePlayer()).toBe(1);
+    });
   });
 });
