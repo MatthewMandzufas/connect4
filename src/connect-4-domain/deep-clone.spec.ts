7edbd4becab99ce60a97cee1ba11b2f4f1f5a7e3
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-05-28 13:36:57 +0800

    RED/GREEN: deepClone > should return a deep copy of an object

--- src/connect-4-domain/deep-clone.spec.ts
+++ src/connect-4-domain/deep-clone.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { describe, it, expect } from "vitest";
 import deepClone from "@/connect-4-domain/deep-clone";
 
@@ -6,11 +7,15 @@ describe("deepClone", () => {
     expect(deepClone(2)).toBe(2);
   });
   it("should return a deep copy of an array", () => {
-    const originalArray: (object | number[] | number)[] = [{ a: 1 }, 2, [3, 4]];
+    const originalArray: [{ a: number }, number, number[]] = [
+      { a: 1 },
+      2,
+      [3, 4],
+    ];
     const clonedArray = deepClone(originalArray);
     expect(clonedArray).not.toBe(originalArray);
     expect(clonedArray[0]).not.toBe(originalArray[0]);
-    expect(clonedArray[0]["a"]).toStrictEqual(originalArray[0]["a"]);
+    expect(clonedArray[0].a).toStrictEqual(originalArray[0].a);
     expect(Object.keys(clonedArray[0]).length).toEqual(1);
     expect(Object.hasOwn(clonedArray[0], "a")).toBeTruthy;
     expect(clonedArray[1]).toStrictEqual(originalArray[1]);
@@ -18,5 +23,21 @@ describe("deepClone", () => {
     expect(clonedArray[2]).not.toBe(originalArray[2]);
     expect(clonedArray[2][0]).toBe(originalArray[2][0]);
     expect(clonedArray[2][1]).toBe(originalArray[2][1]);
+  });
+  it("should return a deep copy of an object", () => {
+    const originalObject: {
+      a: number;
+      b: { c: string; d: number };
+      e: number[];
+    } = { a: 42, b: { c: "hi", d: 9 }, e: [3, 4] };
+    const clonedObject = deepClone(originalObject);
+    expect(clonedObject).not.toBe(originalObject);
+    expect(clonedObject.a).toStrictEqual(originalObject.a);
+    expect(clonedObject.b).not.toBe(originalObject.b);
+    expect(clonedObject.b.c).toStrictEqual(originalObject.b.c);
+    expect(clonedObject.b.d).toStrictEqual(originalObject.b.d);
+    expect(clonedObject.e).not.toBe(originalObject.e);
+    expect(clonedObject.e[0]).toStrictEqual(originalObject.e[0]);
+    expect(clonedObject.e[1]).toStrictEqual(originalObject.e[1]);
   });
 });
