af4f378cc20e53c93f6e60f677294cd4aecce7f8
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-06-04 15:14:37 +0800

    GREEN: parseAsciiTable > given a table with 1 row and 1 column, returns a 1x1 grid

--- src/connect-4-domain/parse-ascii-table.ts
+++ src/connect-4-domain/parse-ascii-table.ts
@@ -1,5 +1,30 @@
-function parseAsciiTable(asciiTable: string): [] {
-  return asciiTable.length === 0 ? [] : [];
+/* eslint-disable @typescript-eslint/no-explicit-any */
+function parseAsciiTable<T>(asciiTable: string): Array<Array<T>> {
+  if (asciiTable.length === 0) {
+    return [];
+  }
+
+  const asciiTableRows = asciiTable.split("\n").slice(1);
+  const gridFormat = asciiTableRows.reduce(
+    (
+      grid: Array<Array<T>>,
+      currentRow: string,
+      currentIndex: number
+    ): Array<Array<T>> => {
+      if (currentIndex % 2 === 0) {
+        return grid;
+      }
+      const rowCells = currentRow.split("|");
+      if (rowCells[1].trim().length === 0) {
+        grid.push([undefined as T]);
+      }
+
+      return grid;
+    },
+    []
+  );
+
+  return gridFormat;
 }
 
 export default parseAsciiTable;
