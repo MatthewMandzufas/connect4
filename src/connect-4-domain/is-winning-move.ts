9caa290a916a66d03e27e6918e0f67adae7914b0
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-06-05 14:35:06 +0800

    GREEN: isWinningMove > given a board and the next players move > and the winning column does not touch the board > and the players move results in a vertical win > detects the win

--- src/connect-4-domain/is-winning-move.ts
+++ src/connect-4-domain/is-winning-move.ts
@@ -4,7 +4,15 @@ function isVerticalWin(
   board: Board,
   playerMove: PlayerMove
 ): { isWin: boolean } {
-  const threeCellsBelowPlayerMove = board.slice(-4, -1);
+  if (board.length < 4) {
+    return {
+      isWin: false,
+    };
+  }
+  const threeCellsBelowPlayerMove = board.slice(
+    playerMove.targetCell.row - 4,
+    playerMove.targetCell.row - 1
+  );
   const isWin = threeCellsBelowPlayerMove.reduce(
     (isWinningMove: boolean, currentRow: BoardCell[]): boolean => {
       return (
